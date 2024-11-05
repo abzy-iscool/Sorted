234ca37a6a6046b925dbea6411530afe963ff8f6
Abhishek J <abhishekjani960@gmail.com>
2024-11-05 20:28:07 +0300

    Delete app directory

--- app/src/main/java/com/example/sorted/presentation/screens/detail_screens/House_Card_Detail_screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sorted.presentation.screens.detail_screens
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun House_Card_Detail_screen() {
-
-}
--- app/src/main/java/com/example/sorted/presentation/screens/detail_screens/Your_Home_screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sorted.presentation.screens.detail_screens
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun Your_Home_Screen() {
-
-}
